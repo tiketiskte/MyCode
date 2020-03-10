3f0a131d4b033a504f1e274331a973adb1224e8e
15092524952@163.com <15092524952@163.com>
2020-03-10 20:27:38 +0800

    all

--- .vscode/test.cpp
+++ .vscode/test.cpp
@@ -1,39 +1,58 @@
-/**
-* Copyright(c)
-* Author : tiketiskte
-*/
-#include <stdio.h>
-#include <stdlib.h>
-//#define IOS {ios::sync_with_stdio(false);cin.tie(0);}
+#include <bits/stdc++.h>
 
-//using namespace std;
-void f(int arr[], int n)
+using namespace std;
+
+
+#define ARRAYLENGTH 40
+
+
+void InsertionSort(int * nums, int arrayLength);
+
+int main(void)
 {
+	int nums1[ARRAYLENGTH];
+
+	for (int i = 0; i < ARRAYLENGTH; i++)
+	{
+		nums1[i] = rand() % 500;
+	}
+	cout << "arraylength = " << ARRAYLENGTH << endl;
+	cout << "Before Sorting" << endl;
+	for (int i = 0; i < ARRAYLENGTH; i++)
+	{
+		cout << nums1[i] << "\t";
+	}
+	cout << endl;
+
+
+	InsertionSort(nums1, ARRAYLENGTH);
+
+	cout << "After Insertion Sorting" << endl;
+	for (int i = 0; i < ARRAYLENGTH; i++)
+	{
+		cout << nums1[i] << "\t";
+	}
+	cout << endl;
+
+
+	getchar();
+    system("pause");
+    return 0;
+}
+
+void InsertionSort(int * numList, int arrayLength)
+{
+	// your code here
     int i, j;
-    for(i = 1; i <= n; i++)
+    for(i = 0; i <= arrayLength; i++)
     {
-        int temp = arr[i];
-        for(j = i; arr[j - 1] > temp; j-- )
+        int temp = numList[i];
+        for(j = i; numList[j - 1] > temp; j-- )
         {
-            arr[j] = arr[j - 1];
+            numList[j] = numList[j - 1];
         }
-        arr[j] = temp;
+        numList[j] = temp;
     }
 }
-int main()
-{
-   int n, a[100];
-   printf("please input n:\n");
-   scanf("%d", &n);
-   for(int i = 1; i <= n; i++)
-   {
-       scanf("%d", &a[i]);
-   }
-   f(a, n);
-   for(int i = 1; i <= n; i++)
-   {
-       printf("%d", a[i]);
-   }
-   system("pause");
-   return 0;
-}
+
+
