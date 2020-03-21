8f05e7af34b1320f2e37b349dd532e154f9e7338
15092524952@163.com <15092524952@163.com>
2020-03-21 23:38:11 +0800

    2020-3-21

---
+++ Codeforces/CF_628_A.cpp
@@ -0,0 +1,24 @@
+/**
+* Copyright(c)
+* Author : tiketiskte
+*/
+#include <bits/stdc++.h>
+#define IOS {ios::sync_with_stdio(false);cin.tie(0);}
+#define INF 0x3f3f3f3f
+
+using namespace std;
+
+int t, x;
+int main()
+{
+   IOS
+   cin >> t;
+   while (t--)
+   {
+       /* code */
+       cin >> x;
+       cout << 1 << " " << x - 1 << endl;
+   }
+   //system("pause");
+   return 0;
+}
