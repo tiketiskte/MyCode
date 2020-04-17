5d7a9b1d9d4304a0518d78958265db41db1fca1c
tiketiskte <15092524952@163.com>
2020-04-17 21:34:10 +0800

    CF

--- Codeforces/CF_632/A.cpp
+++ Codeforces/CF_632/A.cpp
@@ -17,7 +17,7 @@ int main()
        cin >> n >> m;
        for(int i = 1; i <= n; i++) {
            for(int j = 1; j <= m; j++) {
-               if(i == 1 && j == 1) {
+               if(i == 1 && j == m) {
                    cout <<'W';
                } else {
                    cout <<'B';
--- Codeforces/CF_632/B.cpp
+++ Codeforces/CF_632/B.cpp
@@ -9,26 +9,56 @@
 
 using namespace std;
 int t, n, x;
-vector<int>a,b;
+//vector<int>a,b;
+const int maxn = 1e5 + 5;
+int a[maxn], b[maxn];
 bool solve() {
-    
+    bool flag = false, x1 = false, x2 = false;
+    for(int i = 1; i <= n; i++) {
+        if(a[i] < b[i] && !x1) {
+            flag = true;
+            break;
+        } else if(a[i] > b[i] && !x2) {
+            flag = true;
+            break;
+        }
+        if(a[i] == 1) {
+            x1 = true;
+        } else if(a[i] == -1) {
+            x2 = true;
+        }
+    }
+    return flag;
 }
 int main()
 {
    IOS
    cin >> t;
    while(t--) {
+       //int x;
+       memset(a, 0, sizeof(a));
+       memset(b, 0, sizeof(b));
        cin >> n;
        for(int i = 1; i <= n; i++) {
-           cin >> a[i];
+          /*  cin >> x;
+           a.push_back(x); */
+           cin >> a[i]; 
        }
        for(int i = 1; i <= n; i++) {
-           cin >> b[i];
+          /*  cin >> x;
+           b.push_back(x); */
+           cin >> b[i]; 
        }
+       /* for(int x : a) {
+           cout << x << " ";
+       }
+       for(int x : b) {
+           cout << x << " ";
+       } */
        if(solve()) {
-           cout << "YES" << endl;
+           puts("NO");
        } else {
-           cout << "NO" << endl;
+           puts("YES");
        }
    }
    system("pause");
