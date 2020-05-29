1b52b3e686ec859aa00e93d94932afd5afd278d8
tiketiskte <15092524952@163.com>
2020-05-29 10:26:30 +0800

    pair

--- NowCoder/First/NC25136.cpp
+++ NowCoder/First/NC25136.cpp
@@ -1,7 +1,7 @@
 /**
 * Copyright(c)
 * Author : tiketiskte
-*/
+**/
 #include <bits/stdc++.h>
 #define IOS {ios::sync_with_stdio(false);cin.tie(0);}
 #define ll long long
@@ -9,35 +9,31 @@
 #define INF 0x3f3f3f3f
 
 using namespace std;
+
 const int maxn = 32000 + 5;
-int N;
-struct node
-{
-    int l, r;
-    /* data */
-}a[maxn];
-bool cmp(node a, node b) {
-    if(a.r == b.r) {
-        return a.l < b.l;
+int n, len;
+pair<int, int> pr[maxn];
+
+bool cmp(const pair<int, int>& a,const pair<int, int>& b) {
+    if(a.second == b.second) {
+        return a.first < b.first;
     }
-    return a.r < b.r;
+    return a.second < b.second;
 }
 int main()
 {
    IOS
-   cin >> N;
-   int x;
-   for(int i = 1; i <= N; i++) {
-       cin >> a[i].l >> x;
-       a[i].r = a[i].l + x;
+   cin >> n;
+   for(int i = 1; i <= n; i++) {
+       cin >> pr[i].first >> len; 
+       pr[i].second = pr[i].first + len;
    }
-   sort(a + 1, a + 1 + N, cmp);
-   int ans = 0;
-   int now = 0;
-   for(int i = 1; i <= N; i++) {
-       if(a[i].l >= now) {
+   sort(pr + 1, pr + 1 + n, cmp);
+   int ans = 0, now = -1;
+   for(int i = 1; i <= n; i++) {
+       if(now <= pr[i].first) {
            ans++;
-           now = a[i].r;
+           now = pr[i].second;
        }
    }
    cout << ans << endl;
