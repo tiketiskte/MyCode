67b090e4a3532a62ed22fe8d7d5aff680af67825
tiketiskte <15092524952@163.com>
2020-05-30 17:55:43 +0800

    玄学

--- NowCoder/First/NC15553.cpp
+++ NowCoder/First/NC15553.cpp
@@ -1,45 +1,51 @@
 /**
 * Copyright(c)
 * Author : tiketiskte
-*/
+**/
 #include <bits/stdc++.h>
 #define IOS {ios::sync_with_stdio(false);cin.tie(0);}
 #define ll long long
 #define SZ(X) (int)X.size()
 #define INF 0x3f3f3f3f
-
+  
 using namespace std;
-
+  
 const int maxn = 2e5 + 5;
-ll T, n, k;
+int T, n, k;
+int a[maxn];
+ll sum[maxn];
+ll l[maxn], r[maxn];
 ll ans;
-ll w[maxn], sum[maxn], a[maxn], b[maxn];
 int main()
 {
    IOS
    cin >> T;
    while(T--) {
-       ans = -INF;
-       for(int i = 1; i <= maxn; i++) {
-           a[i] = -INF;
-           b[i] = -INF;
-       }
        cin >> n >> k;
+       ans = -1e18;
+       //要开1e18 不然83%样例 不能-INF
+       //memset(a, 0, sizeof(a));
+       //memset(sum, 0, sizeof(sum));
+       memset(l, -INF, sizeof(l));
+       memset(r, -INF, sizeof(r));
+       /* for(int i = 1; i <= maxn; i++) {
+           l[i] = r[i] = -INF;//玄学问题
+       }  */
        for(int i = 1; i <= n; i++) {
-           cin >> w[i];
-           sum[i] = sum[i - 1] + w[i]; 
+           cin >> a[i];
+           sum[i] = sum[i - 1] + a[i];
        }
        for(int i = k; i <= n - k; i++) {
-           a[i] = max(sum[i] - sum[i - k], a[i - 1]);
+           l[i] = max(l[i - 1], sum[i] - sum[i - k]);
        }
-       for(int i = n - k + 1; i >= k + 1; --i) {
-           b[i] = max(sum[i + k - 1] - sum[i - 1], b[i + 1]);
+       for(int i = n - k + 1; i >= k + 1; i--) {
+           r[i] = max(r[i + 1], sum[i + k - 1] - sum[i - 1]);
        }
        for(int i = k; i <= n - k; i++) {
-           ans = max(ans, a[i] + b[i + 1]);
+           ans = max(ans, l[i] + r[i + 1]);
        }
        cout << ans << endl;
    }
-   //system("pause");
+   system("pause");
    return 0;
 }
--- NowCoder/First/NC16593.cpp
+++ NowCoder/First/NC16593.cpp
@@ -15,6 +15,7 @@ const int maxn = 1e4 + 5;
 int n;
 int a[maxn], b[maxn], g[maxn], k[maxn];
 int x, y;
+int flag = 0;
 int main()
 {
    IOS
@@ -27,11 +28,16 @@ int main()
        if(x >= a[i] && x <= a[i] + g[i]) {
            if(y >= b[i] && y <= b[i] + k[i]) {
                cout << i << endl;
+               flag = 1;
                return 0;
            }
        }
    }
-   cout << "-1" << endl;
+   if(flag) {
+       return 0;
+   } else {
+       cout << "-1" << endl;
+   }
    system("pause");
    return 0;
 }
