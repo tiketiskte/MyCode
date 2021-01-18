7e8cb93e2b27aebb94a33d7eb28a6b7259dcd08f
15092524952@163.com <15092524952@163.com>
2021-01-18 17:54:12 +0800

    LinkQueue

--- DS/3_4_3.cpp
+++ DS/3_4_3.cpp
@@ -1,22 +1,25 @@
 /**
-队列的链式实现 带头结点
 * Copyright(c)
 * Author : tiketiskte
 **/
 #include <bits/stdc++.h>
+using namespace std;
 #define IOS {ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
 #define rep(i, a, n) for(int i = a; i < n; i++)
 #define per(i, a, n) for(int i = n - 1; i >= a; i--)
-#define ll long long
-#define PII pair<int, int>
-#define PLL pair<ll, ll>
-#define PDD pair<double, double>
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
 #define sqr(x) (x) * (x)
 #define SZ(X) (int)X.size()
 #define all(x) (x).begin(), (x).end()
 #define INF 0x3f3f3f3f
+typedef long long ll;
+typedef pair <int, int> PII;
+typedef pair <ll, ll> PLL;
+typedef pair <double, double> PDD;
 
-using namespace std;
 ll gcd(ll a, ll b) {return b ? gcd(b, a % b) : a;}
 
 typedef struct node1 {
@@ -26,42 +29,43 @@ typedef struct node1 {
 typedef struct node2 {
     LinkNode *front, *rear;
 }LinkQueue;
-void InitLinkQueue(LinkQueue &q) {
-    q.front = q.rear = (LinkNode*)malloc(sizeof(LinkNode));
-    q.front->next = NULL;
+void InitQueue(LinkQueue &q) {
+    q.front = q.rear = NULL;
 }
 bool IsEmpty(LinkQueue q) {
-    if(q.front -> next == NULL) {
-        return true;
-    } else {
-        return false;
-    }
+    return q.front == NULL;
 }
 void PushLinkQueue(LinkQueue &q, int x) {
     LinkNode *s = (LinkNode*)malloc(sizeof(LinkNode));
     s -> data = x;
     s -> next = NULL;
-    q.rear -> next = s;
-    q.rear = s;
+    if(q.front == NULL) {
+        q.front = s;
+        q.rear = s;
+    } else {
+        q.rear -> next = s;
+        q.rear = s;
+    }
 }
 bool PopLinkQueue(LinkQueue &q, int &e) {
     if(IsEmpty(q)) {
-        return false;
+        return true;
     }
-    LinkNode *s = q.front -> next;
+    LinkNode *s = q.front;
     e = s -> data;
-    q.front -> next = s -> next; // front 指针指向的节点的next 更新到 front 指针指向的节点的下一个节点
+    q.front = s -> next;
     if(q.rear == s) {
-        q.rear = q.front;
+        q.front = NULL;
+        q.rear = NULL;
     }
     free(s);
     return true;
 }
 int main(void) {
     IOS
-    int n, e, x;
+    int n, x, e;
     LinkQueue Q;
-    InitLinkQueue(Q);
+    InitQueue(Q);
     if(IsEmpty(Q)) {
         cout << "The queue is empty!" << endl;
     } else {
