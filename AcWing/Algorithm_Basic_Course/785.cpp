831ef6b71d3b3e46f041319130b5502a3a214510
15092524952@163.com <15092524952@163.com>
2021-02-19 15:16:31 +0800

    qwq

--- AcWing/Algorithm_Basic_Course/785.cpp
+++ AcWing/Algorithm_Basic_Course/785.cpp
@@ -47,7 +47,7 @@ int main(void) {
         cin >> v[i];
     }
     quick_sort(v, 0, n - 1);
-    for(int i = 1; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         cout << v[i] << " ";
     }
     cout << endl;
