5baebf6677e6d1c64a4d3507a1d3be556324dc7b
15092524952@163.com <15092524952@163.com>
2021-08-15 23:25:27 +0800

    chapter2_1

--- Data_Structure/2_Chapter/Practise.cpp
+++ Data_Structure/2_Chapter/Practise.cpp
@@ -2,7 +2,7 @@
 /*
 1.InitList(&L):初始化表.构造一个空的线性表
 2.Length(L):求表长.返回线性表L的长度，即L中数据元素的个数
-3.LocateElem(L, e):按值查找操作.在表L中查找具有给定关键子值的元素
+3.LocateElem(L, e):按值查找操作.在表L中查找具有给定关键字值的元素
 4.GetElem(L, i):按位查找操作.获取表L中第i个位置的元素的值
 5.ListInsert(&L, i, e):插入操作.在表L中的第i个位置上插入指定元素e
 6.ListDelete(&L, i, &e):删除操作.删除表L中第i位置的元素 并用e返回删除元素的值
@@ -67,29 +67,87 @@ int LocateElem(SqList L, int e) {
 }
 //4.GetElem(L, i):按位查找操作.获取表L中第i个位置的元素的值
 int GetElem(SqList L, int i) {
-    int cnt = 0;
+    int cnt = 1;//cnt为元素位置 不是下标
+    //令cnt = 1,从第1个位置开始 cnt = i即找到指定元素位置,下标-1即为元素
     while(cnt < i) {
         cnt++;
     }
-    return L.data[cnt];
+    return L.data[cnt - 1];
+}
+//5.ListInsert(&L, i, e):插入操作.在表L中的第i个位置上插入指定元素e
+bool ListInsert(SqList &L, int i, int e) {
+    if(i <= 0 || i > L.length + 1) {
+        return false;
+    }
+    if(i >= InitSize) {
+        return false;
+    }
+    for(int j = L.length; j >= i; j--) {
+        L.data[j] = L.data[j - 1];
+    }
+    L.data[i - 1] = e, L.length++;
+    return true;
+}
+//6.ListDelete(&L, i, &e):删除操作.删除表L中第i位置的元素 并用e返回删除元素的值
+bool ListDelete(SqList &L, int i, int &e) {
+    if(i <= 0 || i > L.length) {
+        return false;
+    }
+    e = L.data[i - 1];
+    for(int j = i; j < L.length; j++) {
+        L.data[j - 1] = L.data[j];
+    }
+    L.length--;
+    return true;
 }
 //7.PrintList(L):输出操作.按前后顺序输出线性表L的所有元素值
 void PrintList(SqList L) {
     cout << "L's element:" << endl;
     for(int i = 0; i < L.length; i++) {
-        cout << L.data[i] << endl;
+        cout << L.data[i] << " ";
     }
+    cout << endl;
+}
+//8.Empty(L):判空操作.若L为空表，则返回true 否则返回false
+bool Empty(SqList L) {
+    if(L.length == 0) {
+        return true;
+    }
+    return false;
+}
+//9.DestroyList(&L):销毁操作.销毁线性表 并释放线性表L所占用的内存空间
+void DestroyList(SqList &L) {
+    delete L.data;
+    L.length = 0;   
 }
 int main(void) {
     IOS
     SqList L;
-    int e1, e2 = 4;
-    cin >> n >> e1;
+    int e1, e2, e3;
+    int i_1;
+    cout << Empty(L) << endl;
+    cin >> n;
     InitList(L);
     cout << "L's length:" << Length(L) << endl;
     PrintList(L);
+    cin >> e1 >> i_1;//输出查找给定关键字值的元素以及查找第i个位置的元素
     cout << "L's find locate element:" << LocateElem(L, e1) << endl;
-    cout << "L's find key element:" << GetElem(L, e2) << endl;
+    cout << "L's find key element:" << GetElem(L, i_1) << endl;
+    cin >> e2;
+    cout << ListInsert(L, 2, e2)<< endl;
+    PrintList(L);
+    cout << ListDelete(L, 2, e3) << endl;
+    cout << e3 << endl;
+    PrintList(L);
+    DestroyList(L);
+    cout << Empty(L) << endl;
     system("pause");
     return 0;
 }
+/*
+input:
+20
+203 9
+999
+5
+*/
