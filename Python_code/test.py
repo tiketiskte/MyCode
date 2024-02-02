467d7812b689175ce0276900d8d71e4fc447e94e
tiketiskte <15092524952@163.com>
2024-02-02 17:39:27 +0800

    pytorch

--- Python_code/test.py
+++ Python_code/test.py
@@ -1,8 +1,35 @@
-n, m = (int)input().split()
-l = pow(2, n)
-r = pow(2, m)
-ans = 0
-for i in range(l, r):
-    if i % 7 == 1:
-        ans += 1
-print(ans)
+import torch
+from torch import nn
+
+x_data = torch.tensor([[1.0], [2.0], [3.0]])
+y_data = torch.tensor([[2.0], [4.0], [6.0]])
+
+class LinearModel(nn.Module):
+    def __init__(self):
+        # python2.x
+        # super(LinearModel, self).__init__() 
+        super().__init__()
+        self.linear = nn.Linear(1, 1)
+    def forward(self, x):
+        y_pred = self.linear(x)
+        return y_pred
+
+model = LinearModel()
+criterion = nn.MSELoss(reduction='sum')
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+for epoch in range(100):
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+print('w = ', model.linear.weight.item())
+print('b = ', model.linear.bias.item())
+
+x_test = torch.tensor([[4.0]])
+y_test = model(x_test)
+print('y_pred = ', y_test.item())
