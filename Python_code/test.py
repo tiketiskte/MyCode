4239090ebc5fbaf58e5054929b74edce30d77238
tiketiskte <15092524952@163.com>
2024-02-04 22:11:51 +0800

    softmax&MNIST

--- Python_code/test.py
+++ Python_code/test.py
@@ -1,35 +1,12 @@
 import torch
-from torch import nn
+import numpy as np
 
-x_data = torch.tensor([[1.0], [2.0], [3.0]])
-y_data = torch.tensor([[2.0], [4.0], [6.0]])
 
-class LinearModel(nn.Module):
-    def __init__(self):
-        # python2.x
-        # super(LinearModel, self).__init__() 
-        super().__init__()
-        self.linear = nn.Linear(1, 1)
-    def forward(self, x):
-        y_pred = self.linear(x)
-        return y_pred
+Y = torch.LongTensor([2, 0, 1])
+Y_pred1 = torch.Tensor([[0.1, 0.2, 0.9],
+                        [1.1, 0.1, 0.2],
+                        [0.2, 2.1, 0.1]])
 
-model = LinearModel()
-criterion = nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-
-for epoch in range(100):
-    y_pred = model(x_data)
-    loss = criterion(y_pred, y_data)
-    print(epoch, loss.item())
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-print('w = ', model.linear.weight.item())
-print('b = ', model.linear.bias.item())
-
-x_test = torch.tensor([[4.0]])
-y_test = model(x_test)
-print('y_pred = ', y_test.item())
+criterion = torch.nn.CrossEntropyLoss()
+l1 = criterion(Y_pred1, Y)
+print(torch.max(Y_pred1, dim=1))
