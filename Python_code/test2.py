eb93c1e8ff4fbd7720f0e5b798ce4d9e36abf8c0
tiketiskte <15092524952@163.com>
2024-08-17 23:02:11 +0800

    [0817] branch_test

--- Python_code/test2.py
+++ Python_code/test2.py
@@ -1,14 +1,11 @@
+# test
 import torch
-import torch.nn as nn
-from torch.nn import functional as F
-from torch.nn.parameter import Parameter
-from matplotlib import pyplot as plt
-import copy
-import math
+import numpy
+
 def generate_square_subsequent_mask(sz):
-        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-        # mask = mask.float().masked_fill(mask==0, float('-inf')).masked_fill(mask==1, float(0.0))
-        return mask # [sz, sz]
+    mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+    # mask = mask.float().masked_fill(mask==0, float('-inf')).masked_fill(mask==1, float(0.0))
+    return mask  # [sz, sz]
 
 ans = generate_square_subsequent_mask(sz=5)
 print(ans)
